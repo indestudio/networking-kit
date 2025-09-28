a949de03296d1a64cac99e3fd0ffb87193daf34e
madnankhan56 <madnankhan9848@gmail.com>
2025-09-28 16:56:46 +0500

    Provide empty Flipper interceptor for release builds
    This commit introduces a `FlipperInterceptorFactory` specifically for release builds. This factory returns `null` for the `createInterceptor` method, effectively disabling Flipper integration in production environments.

---
+++ networking-kit/src/release/java/com/indiedev/networking/FlipperInterceptorFactory.kt
@@ -0,0 +1,11 @@
+package com.indiedev.networking
+
+import android.content.Context
+import okhttp3.Interceptor
+
+internal object FlipperInterceptorFactory {
+
+    internal fun createInterceptor(context: Context): Interceptor? {
+        return null
+    }
+}
